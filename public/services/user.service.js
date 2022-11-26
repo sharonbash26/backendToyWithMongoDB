b285d0693e019bcf26b35f38668080904e7b8537
vyaron <vyaron@gmail.com>
2022-11-26 15:08:28 +0200

    keep imgUrl also in sessionStorage

--- public/services/user.service.js
+++ public/services/user.service.js
@@ -76,7 +76,7 @@ async function changeScore(by) {
 
 
 function saveLocalUser(user) {
-    user = {_id: user._id, fullname: user.fullname, score: user.score}
+    user = {_id: user._id, fullname: user.fullname, imgUrl: user.imgUrl, score: user.score}
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
     return user
 }
